a6a92ffaa80270d220066f4a1feb07adcaff8089
Balancer <balancer@balancer.ru>
2016-03-14 21:52:19 +0300

    Ещё компоненты.

---
+++ adapters/mybb18/config.example.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['mybb_infonesy'] = [
+	'push_dir' 			=> '/var/www/sync/infonesy-push-board',
+	'node_uuid'			=> 'ru.balancer.board',
+	'user_uuid_base'	=> 'ru.balancer.board.user.',
+	'topic_uuid_base'	=> 'ru.balancer.board.topic.',
+	'post_uuid_base'	=> 'ru.balancer.board.post.',
+];
